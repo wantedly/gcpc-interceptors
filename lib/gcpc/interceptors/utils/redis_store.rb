c5f1ee7b8ffa65b9fc896922a270298122c09d4d
Nao Minami <south37777@gmail.com>
2019-06-06 14:26:09 +0900

    Add redis store spec

--- lib/gcpc/interceptors/utils/redis_store.rb
+++ lib/gcpc/interceptors/utils/redis_store.rb
@@ -8,24 +8,28 @@ module Gcpc
 
         # @param [String] key
         # @return [String]
+        # @raise Redis::BaseConnectionError
         def get(key)
           @redis.get(key)
         end
 
         # @param [String] key
         # @param [String] val
-        # @param [String] val
-        # @return [String, bool]
+        # @param [Integer, nil] ttl
+        # @return [bool]
+        # @raise Redis::BaseConnectionError
         def set(key, val, ttl: nil)
           if ttl
             @redis.setex(key, ttl, val)
           else
             @redis.set(key, val)
           end
+          true
         end
 
         # @param [String] key
         # @return [bool]
+        # @raise Redis::BaseConnectionError
         def exists(key)
           @redis.exists(key)
         end
---
+++ spec/gcpc/interceptors/utils/redis_store_spec.rb
@@ -0,0 +1,57 @@
+require "spec_helper"
+require "mock_redis"
+require "gcpc/interceptors/utils/redis_store"
+
+describe Gcpc::Interceptors::Utils::RedisStore do
+  let(:redis_store) {
+    Gcpc::Interceptors::Utils::RedisStore.new(redis: redis)
+  }
+  let(:redis) { MockRedis.new }
+
+  after do
+    redis.flushall
+  end
+
+  describe "#get" do
+    context "when data does not exist in redis" do
+      it "returns nil" do
+        expect(redis_store.get("key")).to eq nil
+      end
+    end
+
+    context "when data exists in redis" do
+      before do
+        redis_store.set("key", "val")
+      end
+
+      it "returns nil" do
+        expect(redis_store.get("key")).to eq "val"
+      end
+    end
+  end
+
+  describe "#set" do
+    it "sets data to redis" do
+      expect(redis_store.set("key", "val", ttl: 24 * 3600)).to eq true
+      expect(redis_store.get("key")).to eq "val"
+    end
+  end
+
+  describe "#exists" do
+    context "when data does not exist in redis" do
+      it "returns nil" do
+        expect(redis_store.exists("key")).to eq false
+      end
+    end
+
+    context "when data exists in redis" do
+      before do
+        redis_store.set("key", "val")
+      end
+
+      it "returns nil" do
+        expect(redis_store.exists("key")).to eq true
+      end
+    end
+  end
+end
